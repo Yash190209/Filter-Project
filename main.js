74f33067bfc10f2882cf4a6ae45b4c5fffbba56f
Yash190209 <77391635+Yash190209@users.noreply.github.com>
2021-04-28 10:44:21 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,19 @@
+function preload() {
+  
+}
+
+function setup() {
+  
+}
+
+function modelLoaded() {
+  console.log('PoseNet Is Initialized');
+}
+
+function draw() {
+  
+}
+
+function take_snapshot(){    
+  save('myFilterImage.png');
+}
